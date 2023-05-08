95267d3dfe447a1963850116a8668909ef9b8cab
lschmidtfellner <lschmidtfellner@gmail.com>
2023-05-08 15:37:10 -0400

    functional wireframe

---
+++ slots/script.js
@@ -0,0 +1,12 @@
+// storing variables for fetched document elements
+
+const popOverlay = document.getElementById('pop-overlay')
+
+// rules window elements
+const closeRules = document.getElementById('close-rules')
+const openRules = document.getElementById('open-rules')
+
+// money window elements
+const openMoney = document.getElementById('open-money')
+const closeMoney = document.getElementById('close-money')
+
